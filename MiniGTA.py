548d26ca6898eec575b0dabbfa211b14606db12c
benjaminreyes-hash <matiasmorales@insucoabg.cl>
2026-06-09 14:22:18 -0400

    sasas

---
+++ MiniGTA.py
@@ -0,0 +1,17 @@
+import random
+from abc import ABC,abstractmethod
+
+Class Entidadciudad(ABC):
+    def _init__(self,nombre,x,y):
+        self.nombre = nombre
+        self.x = x
+        self.y = y
+        self.salud = 100
+
+@abstractmethod
+def ejecutar_ia(self):
+
+    pass
+
+Class ciudadano(entidadciudad):
+    def _init__(self,nombre,x,y,ocupacion)
